7b7f454e761fdcaeb4458207d0c659daba9455d9
Fernando Petrulio <f.petrulio@virgilio.it>
2018-09-02 10:55:22 +0200

    Clean Code

--- rete.py
+++ rete.py
@@ -5,33 +5,23 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-
-
-#Inizializzo la CNN
-classifier = Sequential()
-# Step 1 - aggiungiamo un primo layer convoluzionale di tipo ReLu, specifichiamo il numero di filtri (32) e la loro dimensione 3x3
-# specifichiamo inoltre la dimensione delle immagini (128,128,3)
-classifier.add(Conv2D(32, (3, 3), input_shape=(128,128,3), activation='relu'))
-# Step 2 - Aggiungiamo un pooling layer per ridurre la dimensione delle immagini e diamo come parametri la dimensione della matrice (2x2)
-# in modo da perdere pochi pixel e concentrare tutte le features in una regione precisa
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# secondo layer convoluzionale ReLu e successivo pooling layer
-classifier.add(Conv2D(32, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# Step 3 - eseguiamo un'operazione di flattening in modo da ridurre le immagini ad un vettore mono-dimensionale
-classifier.add(Flatten())
-# Step 4 - aggiungiamo un layer fully-connected impostando il numero di neuroni (128)
-classifier.add(Dense(units=128, activation='relu'))
-# Step 5 - inizializziamo l'output layer con 10 neuroni corrispondenti alle 10 classi e utilizzando la
-# funzione di attivazione softmax
-classifier.add(Dense(units=10, activation='softmax'))
-# Compiliamo la CNN
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-# Parte 2 - Facciamo delle operazioni preliminari sulle immagini per evitare overfitting
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale=1. / 255)
 
+#Modello CNN :  Input -> Conv(ReLU) -> MaxPooling -> Conv(ReLU) -> MaxPooling -> Flatten -> Dense(128) -> Dense(10)
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape=(128,128,3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=10, activation='softmax'))
+classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+#Normalizzazione Dataset
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 # andiamo a settare il training set passando la path, la dimensione a cui tutte 
